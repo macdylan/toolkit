d28cfb82d7b098f759da4190061a5d3ecb77a8d8
Santa Zhang <santa1987@gmail.com>
2010-12-18 18:08:55 +0800

    merging bengou_down.py

--- grabber.py
+++ grabber.py
@@ -259,6 +259,83 @@ def grab_download_manhua178(manga_url, **opt):
       traceback.print_exc()
       time.sleep(1)
 
+
+def bengou_down_page(page_url, down_dir, page_id):
+  print page_url, down_dir, page_id
+  page_src = urllib2.urlopen(page_url).read()
+
+  # get pic url:
+  idx = page_src.index('"disp"')
+  idx = page_src.index('http://', idx)
+  idx2 = page_src.index('"', idx)
+  img_url = page_src[idx:idx2]
+  print "[img] %s" % img_url
+
+  # download pic:
+  img_ext = img_url[img_url.rfind("."):]
+  pic_fn = down_dir + os.path.sep + ("%03d" % page_id) + img_ext
+  if os.path.exists(pic_fn):
+    print "[skip] %s" % pic_fn
+  else:
+    try:
+      pic_data = urllib2.urlopen(img_url).read()
+      pic_f = open(pic_fn, "wb")
+      pic_f.write(pic_data)
+      pic_f.close()
+    except HTTPError, e:
+      print "[failure] %s" % pic_fn
+      f = open(pic_fn + ".failed", "w")
+      f.close()
+
+def bengou_down_vol(vol_url, down_dir):
+  print "[vol] %s" % vol_url
+  page_src = urllib2.urlopen(vol_url).read()
+  root_url = vol_url[:vol_url.rfind("/")]
+
+  # get pic tree
+  idx = page_src.index("var pictree")
+  idx2 = page_src.index(";", idx)
+  pictree_src = page_src[(idx + 14):idx2]
+  exec "pictree=%s" % pictree_src
+  counter = 1
+  for pic in pictree:
+    down_page(root_url + "/" + pic, down_dir, counter)
+    counter += 1
+
+
+def grab_download_bengou(index_url, **opt):
+  print "This shall be done!"
+  page_src = urllib2.urlopen(index_url).read()
+  index_root = index_url[:index_url.rfind("/")]
+  # find manga name
+  idx = page_src.index("sectioninfo ")
+  idx = page_src.index("title=", idx)
+  idx2 = page_src.index('"', idx + 7)
+  comic_name = page_src[(idx + 7):idx2].decode("utf-8")
+  print comic_name
+
+  # find the volumes
+  idx = page_src.index("mhlist")
+  idx2 = page_src.index("</div>", idx)
+  mhlist_src = page_src[idx:idx2]
+
+  idx = 0
+  idx2 = 0
+  while True:
+    idx = mhlist_src.find("href", idx2)
+    if idx < 0:
+      break
+    idx2 = mhlist_src.index("target", idx)
+    vol_url = index_root + "/" + mhlist_src[(idx + 6):(idx2 - 2)]
+    idx = mhlist_src.find("span", idx2)
+    idx2 = mhlist_src.index("</span>", idx)
+    vol_name = mhlist_src[(idx + 17):(idx2)].decode("utf-8")
+    print "Vol: %s=%s" % (vol_name, vol_url)
+    down_dir = "bengou" + os.path.sep + comic_name + os.path.sep + vol_name
+    print "mkdir: " + down_dir
+    #util_make_dirs(down_dir)
+    #down_vol(vol_url, down_dir)
+
   
 def grab_download_print_help():
   print "download a manga book"
@@ -270,6 +347,8 @@ def grab_download_real(url, **opt):
     url = "http://" + url
   if url.find("manhua.178.com") >= 0:
     grab_download_manhua178(url, **opt)
+  elif url.find("bengou.com") >= 0:
+    grab_download_bengou(url, **opt)    
   else:
     print "sorry, the manga book site is not supported!"
   
