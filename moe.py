a0306141b7f3bcefba96db8c1354815a8caeaa86
Santa Zhang <santa1987@gmail.com>
2012-02-08 18:35:18 +0800

    minor desc

--- moe.py
+++ moe.py
@@ -1674,6 +1674,7 @@ def moe_backup_all():
     moe_backup_by_rating(1)
     print "Phase 4: backup unrated imags"
     moe_backup_by_rating(None)
+    print "Phase 5: cleanup backup"
     moe_backup_cleanup()
 
 def util_check_image_set_md5(image_root, md5_bin, set_name):
